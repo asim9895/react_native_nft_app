ce5abcc7bc609be97d8745b30b9d6f833992d607
asim <asimjaipuri96@gmail.com>
2022-07-02 17:13:06 +0530

    initial setup

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {useFonts} from 'expo-font'
+import Home from './pages/Home';
 
-export default function App() {
+
+const Stack = createStackNavigator()
+
+const App = () => {
+  const [loaded] = useFonts({
+      InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+      InterLight: require("./assets/fonts/Inter-Light.ttf"),
+      InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+      InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+      InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+  })
+
+  if(!loaded) return null
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false }}>
+    <Stack.Screen name="Home" component={Home} />
+   
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
---
+++ pages/Home.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View , SafeAreaView } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+
+
+    <View style={{flex: 1}}>
+
+    
+    </View>
+
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
