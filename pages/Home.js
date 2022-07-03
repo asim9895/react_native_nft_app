dca856c62fffe9b03d2a66dc51ddda0e2f1526ad
asim <asimjaipuri96@gmail.com>
2022-07-03 16:32:31 +0530

    added search functionality

--- pages/Home.js
+++ pages/Home.js
@@ -1,37 +1,65 @@
-import { StyleSheet, Text, View , SafeAreaView, FlatList } from "react-native";
-import React from "react";
-import {NFTData, COLORS} from '../constants/constants/index'
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { NFTData, COLORS } from "../constants/constants/index";
 import HomeHeader from "../components/HomeHeader";
 import NftCard from "../components/NftCard";
 
 const Home = () => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
+	const [data, setdata] = useState(NFTData);
 
+	const [search_value, setsearch_value] = useState("");
 
-    <View style={{flex: 1}}>
+	const onChange = (value) => {
+		setsearch_value(value);
+		setdata(
+			data.filter((d) => {
+				return d.name.toLowerCase().includes(value.toLowerCase());
+			})
+		);
+	};
 
-    <View style={{zIndex: 0}}>
+	useEffect(() => {
+		if (search_value === "") {
+			setdata(NFTData);
+		}
+	}, [search_value]);
 
-    {/* Static black container for styling  */}
-    <View style={{position: "absolute" , top: 0,bottom: 0,left: 0 , right: 0 , zIndex: -1,
-    backgroundColor: COLORS.primary , height: 300
-  }}>
-    </View>
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={{ flex: 1 }}>
+				<View style={{ zIndex: 0 }}>
+					{/* Static black container for styling  */}
+					<View
+						style={{
+							position: "absolute",
+							top: 0,
+							bottom: 0,
+							left: 0,
+							right: 0,
+							zIndex: -1,
+							backgroundColor: COLORS.primary,
+							height: 300,
+						}}
+					></View>
 
-    {/* List to render nft data on screen */}
-    <FlatList 
-       data={NFTData}
-       renderItem={({item}) => <NftCard nft_data={item} />}
-       keyExtractor={(item) => item.id}
-      showsVerticalScrollIndicator={false}
-      ListHeaderComponent={<HomeHeader />}
-    />
-    </View>
-    </View>
-
-    </SafeAreaView>
-  );
+					{/* List to render nft data on screen */}
+					<FlatList
+						data={data}
+						renderItem={({ item }) => <NftCard nft_data={item} />}
+						keyExtractor={(item) => item.id}
+						showsVerticalScrollIndicator={false}
+						ListHeaderComponent={
+							<HomeHeader
+								search_value={search_value}
+								setsearch_value={setsearch_value}
+								onSearchChange={onChange}
+							/>
+						}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
